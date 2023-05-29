4ca1609364ea1d5a8dae9e41772d4575c128a83e
Bruno Gonçalves Ferreira <brunogoncalvesferreira@outlook.com>
2023-05-28 21:09:58 -0300

    :boom: config styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
+import { Button } from "./components/Button"
+import { ThemeProvider } from "styled-components"
+import { defaultTheme } from "./styles/themes/default"
+
 export function App() {
   return (
-    <>
-      <h1>hello world</h1>
-    </>
+    <ThemeProvider theme={defaultTheme}>
+      <Button variant="primary" />
+      <Button variant="secondary" />
+      <Button variant="danger" />
+      <Button variant="success" />
+    </ThemeProvider>
   )
 }
