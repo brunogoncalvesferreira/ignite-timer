69791d57b8137f9512cb14c512f7e43e79ba929b
Bruno Gonçalves Ferreira <brunogoncalvesferreira@outlook.com>
2023-05-29 20:19:59 -0300

    :boom: create react-router-dom

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,37 @@
+import { Play } from "@phosphor-icons/react"
+import {
+  CountDownContainer,
+  FormContainer,
+  HomeContainer,
+  Separator,
+} from "./styles"
+
+export function Home() {
+  return (
+    <HomeContainer>
+      <form action="">
+        <FormContainer>
+          <label htmlFor="task">Vou trabalhar em</label>
+          <input id="task" />
+
+          <label htmlFor="minutesAmount">durante</label>
+          <input id="minutesAmount" type="number" />
+
+          <span>minutos.</span>
+        </FormContainer>
+
+        <CountDownContainer>
+          <span>0</span>
+          <span>0</span>
+          <Separator>:</Separator>
+          <span>0</span>
+          <span>0</span>
+        </CountDownContainer>
+
+        <button type="submit">
+          <Play size={24} />
+        </button>
+      </form>
+    </HomeContainer>
+  )
+}
